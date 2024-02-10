199e6fc6dd19b25aa3fbdef02de43d7bbad90404
Alen Calakovic <calakovicalen@gmail.com>
2024-02-10 22:05:00 +0100

    added auth middleware

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import userRoutes from './src/routes/user.routes';
 import productRoutes from './src/routes/product.routes';
+import { authenticateUser } from './src/middlewares/auth.middleware';
 
 const app = express();
 app.use(express.json());
+app.use(authenticateUser);
+
 const port = 3000;
 
 app.use('/users', userRoutes);
